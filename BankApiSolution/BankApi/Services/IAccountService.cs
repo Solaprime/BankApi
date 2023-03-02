2ea262e50275c5a7f8a53fcf673b27707b17f017
Adebanjo Oluwasola <adebanjo.oluwasola.37@gmail.com>
2023-03-02 15:18:40 +0100

    Converted to Async await

---
+++ BankApiSolution/BankApi/Services/IAccountService.cs
@@ -0,0 +1,23 @@
+﻿using BankApi.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankApi.Services
+{
+   public interface IAccountService
+    {
+        Task<Account> Authenticate(string AccountNumber, string pin);
+        Task<IEnumerable<Account>> GetAllAccounts();
+        Task<Account> Create(Account account, string Pin, string ConfrimPin);
+       
+
+       Task<Account> GetByID(int Id);
+        Task<Account> GetByAccountNumber(string AccountNUmber);
+
+
+        Task Update(Account account, string pin = null);
+        Task Delete(int Id);
+    }
+}
---
+++ BankApiSolution/BankApi/Services/ITransactionService.cs
@@ -0,0 +1,21 @@
+﻿using BankApi.Common;
+using BankApi.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace BankApi.Services
+{
+   public interface ITransactionService
+    {
+        Task<Response> CreateNewTransaction(Transaction transaction);
+        Task<Response> FindTransactionByDate(DateTime date);
+
+        //Refacto all these string values to a Response 
+        Task<Response> MakeDeposit(string AccountNumber, decimal Amount, string TransactionPin);
+        Task<Response> MakeWithdrawal(string AccountNumber, decimal Amount, string TransactionPin);
+        Task<Response>  MakeFundsTransfer(string fromAccount, string toAccount, decimal Amount, string TransactionPin);
+    }
+}
